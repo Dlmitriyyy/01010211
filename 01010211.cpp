3889f9055da17b2fa89ee69fd1b192a499f8e553
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-14 17:30:20 +0300

    Добавьте файлы проекта.

---
+++ 01010211.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+
+	double r0,r1, r2, r3;
+
+	cout << "Введіть 3 числа через пробіл або з нового рядку: ";	
+	cin >> r1 >> r2 >> r3;
+
+	r0 = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+
+	cout << fixed << setprecision(3) << "Відповідь: " << r0;
+
+	return 0;
+}
